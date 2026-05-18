9e076e2ef42d612c13a5b2567eefc487b28c8884
sham jadhav <ghanshamjadhav2003@gmail.com>
2026-05-19 01:52:44 +0530

    add String utility class and implement First Missing Positive algorithm using cyclic sort

---
+++ Java/src/leetcode/FirstMissingPositive.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+class FirstMissingPositive {
+    public static void main(String[] args) {
+    int[] arr = {1,2,0};
+        FirstMissingPositive fmp = new FirstMissingPositive();
+        System.out.println(fmp.firstMissingPositive(arr));
+    }
+
+    public int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index+1) {
+                return index+1;
+            }
+        }
+        return arr.length+1;
+    }
+
+    static void swap(int[] arr, int second, int first){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
